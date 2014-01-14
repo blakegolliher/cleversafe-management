b6503e97e4d72c3b6f7c0f7d18c35cbc90b42f7c
Blake Golliher <blakegolliher@gmail.com>
2014-01-13 19:16:31 -0800

    Update getVaults.py

--- getVaults.py
+++ getVaults.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-## 
+##
 #
-# Simple report on logical used 
+# Simple report on logical used
 # and physical used space.
-# It should evolve more, just 
+# It should evolve more, just
 # basic report for now.
 ##
 # [blakegolliher@admin001 cleversafe-code]$ ./getVaults.py
@@ -38,9 +38,15 @@ import json, urllib2, os, base64, getpass
 colorred = "\033[01;31m{0}\033[00m"
 colorgrn = "\033[1;36m{0}\033[00m"
 
+def sizeof_fmt(num):
+    for x in ['Bytes','KB','MB','GB','TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
 password = getpass.getpass()
 
-request = urllib2.Request("https://dsnet.manager/manager/api/json/1.0/listVaults.adm")
+request = urllib2.Request("https://17.176.156.23/manager/api/json/1.0/listVaults.adm")
 request.add_header('Accept', 'application/json')
 base64string = base64.encodestring('%s:%s' % ('admin',password)).replace('\n', '')
 request.add_header("Authorization", "Basic %s" % base64string)
@@ -58,6 +64,6 @@ for x in data['responseData']['vaults']:
         phyusedspace.append(x['usedPhysicalSizeFromStorage'])
 
 for name,log,phy in zip(names, logusedspace, phyusedspace):
-        print colorred.format("Vault name: ") + name + colorred.format(".")
-        print colorgrn.format("  logical space used: ") + str(log) + colorgrn.format(" bytes.")
-        print colorgrn.format("  physical space used: ") + str(phy) + colorgrn.format(" bytes.")
+        print colorred.format("Vault name: ") + name
+        print colorgrn.format("  logical space used: ") + sizeof_fmt(log)
+        print colorgrn.format("  physical space used: ") + sizeof_fmt(phy)
