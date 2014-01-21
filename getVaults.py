16fe0356cb2659dc633eb07573eb0b58030616bf
Blake Golliher <blakegolliher@gmail.com>
2014-01-20 18:28:35 -0800

    Update getVaults.py

--- getVaults.py
+++ getVaults.py
@@ -4,9 +4,12 @@
 # and physical used space.
 # It should evolve more, just
 # basic report for now.
+#
+# Assumes only one storage pool.
 ##
 # blake golliher
 # blakegolliher@gmail.com
+#
 ##
 
 import json, urllib2, os, base64, getpass, sys
@@ -15,17 +18,15 @@ usage = """
 Usage: ./getVaults.py manager name (or ip)
 e.g ./getVaults.py dsnet.manager
 
-Vault name: 			vault8
-  logical space used: 		428.7 GB
-  physical space used: 		428.7 GB
-  estimated logical space used: 216.6 TB
+Pool name:			thepool
+   Usable space: 		   5.25 PB
 
-Vault name: 			vault7
-  logical space used: 		300.4 GB
-  physical space used: 		300.4 GB
-  estimated logical space used: 216.6 TB
-
-EXAMPLE
+Vault name:			vault01
+   logical space used:		   758.61 GB
+   physical space used:		   758.61 GB
+Vault name:			vault02
+   logical space used:		   130.14 GB
+   physical space used:		   130.14 GB
 
 """
 
@@ -35,14 +36,12 @@ if len(sys.argv)!=2:
 
 manager = sys.argv[1]
 
-colorred = "\033[01;31m{0}\033[00m"
-colorgrn = "\033[1;36m{0}\033[00m"
-
-def sizeof_fmt(num):
-    for x in ['Bytes','KB','MB','GB','TB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-        num /= 1024.0
+def readable_size(size):
+    for unit in ['bytes','KB','MB','GB','TB']:
+        if size < 1000.0 and size > -1000.0:
+            return "%3.2f %s" % (size, unit)
+        size /= 1000.0
+    return "%3.2f %s" % (size, 'PB')
 
 password = getpass.getpass()
 
@@ -52,21 +51,23 @@ base64string = base64.encodestring('%s:%s' % ('admin',password)).replace('\n', '
 request.add_header("Authorization", "Basic %s" % base64string)
 result = urllib2.urlopen(request)
 
-data = json.load(result)
+data =   json.load(result)
+pname =  data['responseData']['vaults'][0]['storagePools'][0]['storagePool']['name']
+pusize = data['responseData']['vaults'][0]['storagePools'][0]['storagePool']['usableSize']
+
+print '\nPool name:			%s ' % pname
+print '   Usable space: 		   %s \n' % readable_size(pusize)
 
 names = []
 logusedspace = []
 phyusedspace = []
-estusedspace = []
 
 for item in data['responseData']['vaults']:
         names.append(item['name'])
         logusedspace.append(item['usedLogicalSizeFromStorage'])
         phyusedspace.append(item['usedPhysicalSizeFromStorage'])
-	estusedspace.append(item['estimateUsableTotalLogicalSizeFromStorage'])
 
-for name,log,phy,est in zip(names, logusedspace, phyusedspace, estusedspace):
-        print colorred.format("Vault name: 			") + name
-        print colorgrn.format("  logical space used: 		") + sizeof_fmt(log)
-        print colorgrn.format("  physical space used: 		") + sizeof_fmt(phy)
- 	print colorgrn.format("  estimated logical space used:  ") + str(sizeof_fmt(est))
+for name,log,phy in zip(names, logusedspace, phyusedspace):
+        print 'Vault name:			%s ' % name
+        print '   logical space used:		   %s ' % readable_size(log)
+        print '   physical space used:		   %s ' % readable_size(phy)
