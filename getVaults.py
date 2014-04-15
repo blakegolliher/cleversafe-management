af31ced3dceb94a59786b71cfb4c144bb1b36b87
Blake Golliher <blakegolliher@gmail.com>
2014-04-15 15:37:23 -0700

    Update getVaults.py

--- getVaults.py
+++ getVaults.py
@@ -13,18 +13,20 @@
 import json, urllib2, os, base64, getpass, sys
 
 usage = """
+
+EXAMPLE
 Usage: ./getVaults.py manager name (or ip)
-e.g ./getVaults.py dsnet.manager
+[blakegolliher@admin001 ~ ]$ ./getVaults.py dsnet.manager
 
-Vault name: 			vault01
-  logical space used: 		428.7 GB
-  physical space used: 		428.7 GB
-  estimated logical space used: 216.6 TB
+Vault name: video01
+  space used on disk: 		4.990 PB
+  space used to the app :	2.772 PB
 
-Vault name: 			vault02
-  logical space used: 		300.4 GB
-  physical space used: 		300.4 GB
-  estimated logical space used: 216.6 TB
+Vault name: music01
+  space used on disk: 		19.383 GB
+  space used to the app :	10.752 GB
+
+....
 
 """
 
@@ -34,38 +36,29 @@ if len(sys.argv)!=2:
 
 manager = sys.argv[1]
 
-colorred = "\033[01;31m{0}\033[00m"
-colorgrn = "\033[1;36m{0}\033[00m"
-
 def sizeof_fmt(num):
-    for x in ['Bytes','KB','MB','GB','TB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-        num /= 1024.0
-
-password = getpass.getpass()
+    for x in ['Bytes','KB','MB','GB','TB', 'PB']:
+        if num < 1000.0:
+            return "%3.3f %s" % (num, x)
+        num /= 1000.0
 
 request = urllib2.Request("https://" + manager + "/manager/api/json/1.0/listVaults.adm")
 request.add_header('Accept', 'application/json')
-base64string = base64.encodestring('%s:%s' % ('admin',password)).replace('\n', '')
-request.add_header("Authorization", "Basic %s" % base64string)
 result = urllib2.urlopen(request)
 
 data = json.load(result)
 
 names = []
-logusedspace = []
 phyusedspace = []
 estusedspace = []
 
 for item in data['responseData']['vaults']:
         names.append(item['name'])
-        logusedspace.append(item['usedLogicalSizeFromStorage'])
         phyusedspace.append(item['usedPhysicalSizeFromStorage'])
-        estusedspace.append(item['estimateUsableTotalLogicalSizeFromStorage'])
+        estusedspace.append(item['estimateUsableUsedLogicalSizeFromStorage'])
 
-for name,log,phy,est in zip(names, logusedspace, phyusedspace, estusedspace):
-        print colorred.format("Vault name: 			") + name
-        print colorgrn.format("  logical space used: 		") + sizeof_fmt(log)
-        print colorgrn.format("  physical space used: 		") + sizeof_fmt(phy)
-        print colorgrn.format("  estimated logical space used:  ") + str(sizeof_fmt(est))
+for name,phy,est in zip(names, phyusedspace, estusedspace):
+        print "\nVault name: %s " % name
+        print "  space used on disk: 		%s " % sizeof_fmt(phy)
+        print "  space used to the app :	%s " % str(sizeof_fmt(est))
+print "\n"
